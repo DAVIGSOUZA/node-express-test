58275b505e41aebd810512e67e95cd462e17d360
Davi Garcia <davigs@hotmail.com>
2023-04-15 11:27:55 -0300

    add table constants

---
+++ src/db/contants.ts
@@ -0,0 +1,7 @@
+export const purchaseTable = 'purchases'
+
+export const productsTable = 'products'
+
+export const usersTable = 'users'
+
+export const purchasesProductsTable = 'purchases_products'
